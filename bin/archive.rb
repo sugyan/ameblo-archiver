e6da43ebe7d94c0cbfebc2edc90dc079d38d0237
sugyan <sugi1982@gmail.com>
2013-02-25 22:37:09 +0900

    modify

--- bin/archive.rb
+++ bin/archive.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+abort("usage: #{__FILE__} <ameba id>") unless ARGV.size > 0
+
 $LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
 require 'archiver'
 
--- lib/archiver.rb
+++ lib/archiver.rb
@@ -6,37 +6,32 @@ require 'logger'
 class Archiver
   def initialize(dir = 'data')
     @log = Logger.new(STDERR)
-    @output_dir = File.dirname(__FILE__) + '/../' + dir
-    @downloaded = {}
+    @dir = File.dirname(__FILE__) + '/../' + dir
   end
 
   def start(id)
-    @id = id
-    @base_url = "http://ameblo.jp/#{id}/"
-    download_page('entrylist.html')
-  end
+    targets = ['entrylist.html']
+    downloaded = {}
 
-  def download_page(page)
-    @log.info(page)
-    targets = []
-    doc = Nokogiri::HTML(open(@base_url + page))
-    doc.css('a').each do |e|
-      href = e.get_attribute('href')
-      if href && href.match(/#{@id}\/(?:entry-\d+|entrylist-\d+)\.html/)
-        path = URI(href).path.split(/\//).last
-        e.set_attribute('href', './' + path)
-        targets.push(path)
+    while page = targets.shift
+      @log.info(page)
+      doc = Nokogiri::HTML(open("http://ameblo.jp/#{id}/#{page}"))
+      doc.css('a').each do |e|
+        href = e.get_attribute('href')
+        if href && href.match(/#{@id}\/(?:entry-\d+|entrylist-\d+)\.html/)
+          path = URI(href).path.split(/\//).last
+          e.set_attribute('href', './' + path)
+          targets.push(path) unless downloaded[path]
+        end
       end
-    end
+      # save
+      open(@dir + '/' + page, 'wb') do |des|
+        des.write(doc)
+      end
+      downloaded[page] = true
 
-    open(@output_dir + '/' + page, 'wb') do |des|
-      des.write(doc)
-    end
-    @downloaded[page] = true
-    targets.each do |p|
-      next if @downloaded[p]
-      sleep 0.5
-      download_page(p)
+      sleep 1
+      targets = targets.uniq.reject{ |t| downloaded[t] }
     end
   end
 end
